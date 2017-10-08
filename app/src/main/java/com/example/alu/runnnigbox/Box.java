587535c9642d2c4ceb496f32fa3392c80377469a
EvanYang <evansyangs@gmail.com>
2017-10-08 20:54:20 +0800

    Modify the shape of the stage and box

--- app/src/main/java/com/example/alu/runnnigbox/Box.java
+++ app/src/main/java/com/example/alu/runnnigbox/Box.java
@@ -3,6 +3,9 @@ package com.example.alu.runnnigbox;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 
 /**
  * Created by yang on 2017/10/5.
@@ -11,12 +14,13 @@ import android.graphics.Paint;
 public class Box {
     private int x;
     private int y;          //坐标
-    private static int WIDTH = 80;          // 边长
+    private static int WIDTH = 100;          // 边长
     private static int GRAVITY = 1;        //重力加速度
     private int jumpSpeed = 0;              //跳跃初始速度
     public static final int HIGH_SPEED = 30;
     public static final int LOW_SPEED = 20;
     private Boolean isDown = true;
+    private static int RADIAN = 20; //圆角矩阵的弧度
     private Paint mPaint;       //绘制
     GameView gameView;
     public static int getWIDTH() {
@@ -33,7 +37,10 @@ public class Box {
     public Box(GameView gameView, int x, int y){
         this.gameView = gameView;
         mPaint = new Paint();
-        mPaint.setColor(Color.BLACK);
+        mPaint.setColor(Color.GRAY);
+        //设置画出的图形填充的类型,fill为内部填充,stroke为只有边框,内容不填充
+        mPaint.setStrokeWidth(10);//设置边框的宽度. 如矩形的边宽, 文字的宽度. 接收实参为像素单位
+        mPaint.setStyle(Paint.Style.STROKE);
         this.x = x;
         this.y = y;
     }
@@ -124,7 +131,10 @@ public class Box {
         this.jumpSpeed = jumpSpeed;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     void draw(Canvas canvas){
-        canvas.drawRect(x,y,x+WIDTH,y+WIDTH,mPaint);//绘制主人公Box
+        RectF rectF = new RectF(x,y,x+WIDTH,y+WIDTH);
+        canvas.drawRoundRect(rectF,RADIAN,RADIAN,mPaint);//圆角矩形
+        //canvas.drawRect(x,y,x+WIDTH,y+WIDTH,mPaint);//绘制主人公Box
     }
 }
