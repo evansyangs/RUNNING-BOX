8ced363ca3d4fd853fbcfa6f5f974f87d6e61879
MRVERNEY <xu_yaokun@163.com>
2017-10-10 21:51:27 +0800

    return dialog

--- app/src/main/java/com/example/alu/runnnigbox/GameActivity.java
+++ app/src/main/java/com/example/alu/runnnigbox/GameActivity.java
@@ -1,12 +1,19 @@
 package com.example.alu.runnnigbox;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface.OnClickListener;
+
 
 /**
  * Created by 10988 on 2017/10/7.
@@ -29,7 +36,40 @@ public class GameActivity extends Activity {
 
         setContentView(R.layout.activity_game_view);
 
+        Button button =  this.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new Builder(GameActivity.this);
+                builder.setTitle("提示");
+                builder.setMessage("你确定要退出游戏吗");
+
+                builder.setPositiveButton("重新开始", new OnClickListener() {
+                    public void onClick(DialogInterface arg0, int arg1) {
+
+                        Intent intent = new Intent(GameActivity.this, GameActivity.class);
+                        GameActivity.this.startActivity(intent);
+                        GameActivity.this.finish();
+                    }
+                });
+
+                builder.setNegativeButton("退出游戏", new OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        Intent intent = new Intent(GameActivity.this,StartMenu.class);
+                        GameActivity.this.startActivity(intent);
+                        GameActivity.this.finish();
+                    }
+                });
+
+                AlertDialog b = builder.create();
+                b.show();
+            }
+        });
+
+
         mView = (GameView) findViewById(R.id.gameView);//获取gameView资源
+
+
     }
 
     /*
